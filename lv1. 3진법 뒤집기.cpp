fc33c7762d86178e76e68079d14f62157982d8c2
DChoel <ka0518392@gmail.com>
2021-06-29 16:54:27 +0900

    hi hello

--- lv1. 3진법 뒤집기.cpp
+++ lv1. 3진법 뒤집기.cpp
@@ -2,25 +2,7 @@
 #include <vector>
 //#include <iostream>
 using namespace std;
-int pow(int n,int i){
-    int temp=1;
-    for(int k=0;k<i;k++){
-        temp*=n;
-    }
-    return temp;
-}
-
-int solution(int n) {
-    int answer = 0;
-    string temp;
-    char ctmp;
-    for(int i=0;n!=0;n/=3, i++){
-        ctmp = n%3 + '0';
-        temp += ctmp;
-        //cout<<answer<<" "<<n%3<<endl;
-        answer=answer*3 + n%3;
-    }
-
-   // cout<<"temp: "<<temp<<endl;
-    return answer;
+int main(){
+ cout<<"hello world!"<<endl;
+ return 0;
 }
